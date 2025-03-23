29389aaaf21eae939997d33a4e96f117d0d1b0f6
Marco Agostini <107429726+Marukoalitini@users.noreply.github.com>
2025-03-23 16:32:23 -0300

    Ajustar merge

--- public/assets/scripts/cadastro_proprietario.js
+++ public/assets/scripts/cadastro_proprietario.js
@@ -11,11 +11,16 @@ const traducoes = {
         bairro: "Bairro",
         cidade: "Cidade",
         estado: "Estado",
+        numero: "Número",
         cadastrar: "Cadastrar",
         cepInvalido: "CEP inválido! Digite no formato 00000-000.",
         erroObrigatorio: "O campo {campo} é obrigatório.",
         dataInvalida: "Insira uma data de nascimento válida.",
-        numero: "numero"
+        email: "E-mail",
+        senha: "Senha",
+        senhaConfirmacao: "Confirmar Senha",
+        emailInvalido: "Insira um e-mail válido.",
+        telefoneInvalido: "Insira um telefone válido(Deve ter entre 10 e 11 dígitos)."
     }, en: {
         titulo: "Registration",
         cpf: "CPF",
@@ -28,11 +33,16 @@ const traducoes = {
         bairro: "Neighborhood",
         cidade: "City",
         estado: "State",
+        numero: "Number",
         cadastrar: "Register",
         cepInvalido: "Invalid ZIP Code! Use the format 00000-000.",
         erroObrigatorio: "The {campo} field is required.",
-        dataInvalida: "Please enter a valid date of birth."
-
+        dataInvalida: "Please enter a valid date of birth.",
+        email: "E-mail",
+        senha: "Password",
+        senhaConfirmacao: "Confirm Password",
+        emailInvalido: "Please enter a valid e-mail.",
+        telefoneInvalido: "Please enter a valid phone number(Must have between 10 and 11 digits)."
     }
 };
 
@@ -51,17 +61,30 @@ function atualizarIdioma(idioma) {
     document.getElementById('botao-cadastrar').textContent = traducoes[idioma].cadastrar;
     document.getElementById('cep-invalido').textContent = traducoes[idioma].cepInvalido;
     document.getElementById('data-nascimento-invalida').textContent = traducoes[idioma].dataInvalida;
+    document.querySelector('label[for="numero"]').textContent = traducoes[idioma].numero;
+    document.querySelector('label[for="email"]').textContent = traducoes[idioma].email;
+    document.querySelector('label[for="senha"]').textContent = traducoes[idioma].senha;
+    document.querySelector('label[for="senha-confirmacao"]').textContent = traducoes[idioma].senhaConfirmacao;
+    document.getElementById('email-invalido').textContent = traducoes[idioma].emailInvalido;
+    document.getElementById('telefone-invalido').textContent = traducoes[idioma].telefoneInvalido;
+
+
+
     const camposObrigatorios = [{id: 'cpf', campo: traducoes[idioma].cpf}, {
         id: 'nome', campo: traducoes[idioma].nome
-    }, {id: 'sobrenome', campo: traducoes[idioma].sobrenome}, {
-        id: 'telefone', campo: traducoes[idioma].telefone
-    }, {id: 'data-nascimento', campo: traducoes[idioma].dataNascimento}, {
-        id: 'cep', campo: traducoes[idioma].cep
-    }, {id: 'logradouro', campo: traducoes[idioma].rua}, {id: 'bairro', campo: traducoes[idioma].bairro}, {
-        id: 'cidade', campo: traducoes[idioma].cidade
-    }, {
-        id: 'estado', campo: traducoes[idioma].estado
-    }, {id: 'numero', campo: traducoes[idioma].numero}];
+    }, {id: 'sobrenome', campo: traducoes[idioma].sobrenome
+    }, {id: 'telefone', campo: traducoes[idioma].telefone
+    }, {id: 'data-nascimento', campo: traducoes[idioma].dataNascimento
+    }, {id: 'cep', campo: traducoes[idioma].cep
+    }, {id: 'logradouro', campo: traducoes[idioma].rua
+    }, {id: 'bairro', campo: traducoes[idioma].bairro
+    }, {id: 'cidade', campo: traducoes[idioma].cidade
+    }, {id: 'estado', campo: traducoes[idioma].estado
+    }, {id: 'numero', campo: traducoes[idioma].numero
+    }, {id: 'email', campo: traducoes[idioma].email
+    }, {id: 'senha', campo: traducoes[idioma].senha
+    }, {id: 'senha-confirmacao', campo: traducoes[idioma].senhaConfirmacao}
+    ];
 
     camposObrigatorios.forEach(({id, campo}) => {
         const erroElemento = document.getElementById(`${id}-erro`);
@@ -88,6 +111,7 @@ function bloquearCampo(id, valor) {
         campo.removeAttribute('readonly');
     }
 }
+
 function getDataAtual() {
     const hoje = new Date();
     const ano = hoje.getFullYear();
@@ -165,6 +189,38 @@ function validarDataNascimento() {
 
 }
 
+function validarTelefone() {
+    const telefone = document.getElementById('telefone').value;  // Obtendo o valor do telefone
+    const telefoneError = document.getElementById('telefone-invalido'); // Obtendo o elemento do erro
+
+    // Remove todos os caracteres não numéricos
+    const telefoneNumeros = telefone.replace(/\D/g, ''); // Remove caracteres não numéricos
+
+    // Verifica se o telefone tem entre 10 e 11 dígitos após remoção dos não numéricos
+    if (telefoneNumeros.length < 10 || telefoneNumeros.length > 11) {
+        telefoneError.classList.remove('hidden'); // Exibe a mensagem de erro
+        return false; // Impede o envio do formulário
+    }
+    telefoneError.classList.add('hidden');
+    return true;
+}
+
+function validarEmail() {
+    const email = document.getElementById('email');
+    const emailInvalido = document.getElementById('email-invalido');
+    const emailValue = email.value.trim();
+
+    // Regex para validar o formato do e-mail
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!regexEmail.test(emailValue)) {
+        emailInvalido.classList.remove('hidden');
+        return false;
+    }
+    emailInvalido.classList.add('hidden'); // Esconde a mensagem de erro se o e-mail for válido
+    return true;
+}
+
 document.getElementById('cep').addEventListener('input', function () {
     let cepFormatado = this.value.replace(/\D/g, '').slice(0, 8);
 
@@ -182,10 +238,26 @@ document.getElementById('cep').addEventListener('input', function () {
 
 document.getElementById('cep').addEventListener('blur', buscarEndereco);
 
+document.getElementById('telefone').addEventListener('input', function (e) {
+    this.value = this.value.replace(/\D/g, '');
+    let value = e.target.value.replace(/\D/g, ''); // Remove tudo que não é dígito
+    if (value.length > 11) value = value.slice(0, 11); // Limita a 11 dígitos
+
+    // Aplica a máscara (XX) XXXX-XXXX ou (XX) XXXXX-XXXX
+    if (value.length > 2) {
+        value = `(${value.slice(0, 2)}) ${value.slice(2)}`;
+    }
+    if (value.length > 10) {
+        value = `${value.slice(0, 10)}-${value.slice(10)}`;
+    }
+
+    e.target.value = value; // Atualiza o valor do campo
+});
+
 document.getElementById('form-cadastro').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const camposObrigatorios = ["logradouro", "cidade", "bairro", "estado", "cep", "data-nascimento", "telefone", "sobrenome", "nome", "cpf", "numero"];
+    const camposObrigatorios = ["senha-confirmacao","senha","numero","logradouro", "cidade", "bairro", "estado", "cep","email", "data-nascimento", "telefone", "sobrenome", "nome", "cpf"];
     let formularioValido = true;
 
     camposObrigatorios.forEach(id => {
@@ -205,6 +277,14 @@ document.getElementById('form-cadastro').addEventListener('submit', function (ev
     }
     if (!buscarEndereco()) {
         formularioValido = false
+    }
+
+    if (!validarTelefone()) {
+        formularioValido = false;
+    }
+
+    if (!validarEmail()) {
+        formularioValido = false;
     }
 
     if (formularioValido) {
